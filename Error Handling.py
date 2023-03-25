a56a5c6c698f5d5c10feb2f0fb6e2645e3e43b1f
Pylos7 <nestor1ingles@gmail.com>
2023-03-24 20:01:55 -0400

    Added Decorators and first Error Handling

---
+++ Error Handling.py
@@ -0,0 +1,22 @@
+# Error Handling in Python
+
+# 1. Syntax Errors
+    # Syntax errors are perhaps the most common kind of complaint you get while you are still learning Python:
+# 2. Exceptions
+    # Even if a statement or expression is syntactically correct, it may cause an error when an attempt is made to execute it.
+# 3. Handling Exceptions
+    # The statements try and except can be used to handle selected exceptions. A try statement may have more than one except clause, to specify handlers for different exceptions.
+# 4. Raising Exceptions
+    # The raise statement allows the programmer to force a specified exception to occur.
+# 5. User-Defined Exceptions
+    # Programmers can name their own exceptions by creating a new exception class.
+
+try:
+    age = int(input("Enter your age: "))
+    10/age
+except ValueError:
+    print("Invalid input - Please enter a number")
+except ZeroDivisionError:
+    print("Age cannot be 0")
+else:
+    print("Thank you!")
