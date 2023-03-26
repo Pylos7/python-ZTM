d59bb1c9d4e41f260c97963de52370a273769f35
Pylos7 <nestor1ingles@gmail.com>
2023-03-26 12:57:34 -0400

    Added random_game.py

--- random_game.py
+++ random_game.py
@@ -1,18 +1,25 @@
-import sys
+# Description: A simple number guessing game
+
+import sys # to get command line arguments
 from random import randint
 
-num1 = int(sys.argv[1])
+num1 = int(sys.argv[1]) # first argument 
 
 num2 = int(sys.argv[2])
 
-random_number = randint(num1, num2)
+answer = randint(num1, num2)
 
-guess = int(input(f"Hello World! Guess a number between {num1} and {num2}: "))
+while True: # infinite loop
+    try:
+        guess = int(input(f"Guess a number between {num1} and {num2}: "))
+        if num1 <= guess <= num2: # check if guess is between num1 and num2
+            if guess == answer:
+                print("You're a genius!")
+                break
+            else:
+                print("Sorry, try again!")
+        else:
+            print(f"hey, {guess} is not between {num1} and {num2}")
+    except ValueError: # if user enters a string
+        print("Please enter a number!")
 
-
-
-while random_number != guess:
-    guess = int(input("You are unlucky! try again! "))
-else:
-    print("You are a genius!")
-    
