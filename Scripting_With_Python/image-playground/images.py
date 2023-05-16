4a55c5869b7bf90f333718b058ced9d9df97c9e0
Pylos7 <nestor1ingles@gmail.com>
2023-05-16 12:06:17 -0400

    Added image_playground

--- Scripting_With_Python/image-playground/images.py
+++ Scripting_With_Python/image-playground/images.py
@@ -1,5 +1,7 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 
-img = Image.open('./Pokedex/pikachu.jpg')
+img = Image.open('./astro.jpg')
+img.thumbnail((400, 400)) # Good for creating profile pictures
+img.save("thumbnail.jpg") # Keeps the aspect ratio under the specified parametters
 
-print(img.format)
+print(img.size)
